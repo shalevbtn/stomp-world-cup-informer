d0a0abf8100afa247575d7e68310c7e86a0a9a07
nitzan101 <nehorayn@post.bgu.ac.il>
2026-01-08 18:31:49 +0200

    encDec impl

---
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompMessageEncoderDecoder.java
@@ -0,0 +1,39 @@
+package bgu.spl.net.impl.stomp;
+
+import java.nio.charset.StandardCharsets;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import java.util.Arrays;
+
+public class StompMessageEncoderDecoder implements MessageEncoderDecoder<String> {
+
+    private byte[] bytes = new byte[1 << 10]; // 1k
+    private int len = 0;
+
+    @Override
+    public String decodeNextByte(byte nextByte) { 
+        if (nextByte == '\u0000')
+            return popString();
+        pushByte(nextByte);
+        return null; 
+    }
+
+    @Override
+    public byte[] encode(String message) {
+        return (message + "\u0000").getBytes();
+    }
+
+    private String popString(){
+        String result = new String(bytes, 0, len); 
+        len = 0;
+        return result;
+    }
+
+    private void pushByte(byte nextByte){
+        if (len >= bytes.length)
+            bytes = Arrays.copyOf(bytes, len << 1);
+        bytes[len++] = nextByte;
+    }
+    
+
+}
